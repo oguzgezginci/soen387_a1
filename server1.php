665d556b2ee61834ce1534468e2b1eac44a98d9c
Jerbi Malek <malek.jerbi@gmail.com>
2022-10-08 20:03:28 -0300

    names modification

--- server1.php
+++ server1.php
@@ -1,11 +1,11 @@
-		
+
 		<!DOCTYPE html>
 		<html xmlns = "http://www.w3.org/1999/xhtml">
 		   <head>
 		      <title>Search Results</title>
 		   <style type = "text/css">
 		         body  { font-family: arial, sans-serif;
-		                 background-color: #F0E68C } 
+		                 background-color: #F0E68C }
 		         table { background-color: #ADD8E6 }
 		         td    { padding-top: 2px;
 		                 padding-bottom: 2px;
@@ -18,26 +18,26 @@
 		   <body>
 		      <?php
 		         extract( $_POST );
-		
+
 		         // build SELECT query
-		      
+
 				 $query="INSERT INTO books (ID,Title,Category,ISBN)
 				 VALUES ('$ID','$Title','$Category','$ISBN')";
-				 
-				 
+
+
 		         // Connect to MySQL
 		         if ( !( $database = mysqli_connect( "localhost",
-		            "root", "" ) ) )                      
+		            "root", "" ) ) )
 		            die( "Could not connect to database </body></html>" );
-		   
+
 		         // open Products database
-		         if ( !mysqli_select_db( $database ,"products" ) )
+		         if ( !mysqli_select_db( $database ,"assignment1" ) )
 		            die( "Could not open products database </body></html>" );
-		     
-		
-		
+
+
+
 		         // query Products database
-		         if ( !( $result = mysqli_query( $database,$query) ) ) 
+		         if ( !( $result = mysqli_query( $database,$query) ) )
 		         {
 		            print( "Could not execute query! <br />" );
 		            die( mysqli_error() . "</body></html>" );
@@ -48,6 +48,6 @@
 				}
 		         mysqli_close( $database );
 		      ?><!-- end PHP script -->
-		      
+
 		   </body>
 		</html>
--- server2.php
+++ server2.php
@@ -1,13 +1,13 @@
-		
+
 		<!DOCTYPE html >
-		
-		
+
+
 		<html>
 		   <head>
 		      <title>Search Results</title>
 		   <style type = "text/css">
 		         body  { font-family: arial, sans-serif;
-		                 background-color: #F0E68C } 
+		                 background-color: #F0E68C }
 		         table { background-color: #ADD8E6 }
 		         td    { padding-top: 2px;
 		                 padding-bottom: 2px;
@@ -20,25 +20,25 @@
 		   <body>
 		      <?php
 		         extract( $_POST );
-		
+
 		         // build SELECT query
 		         $query = "SELECT " . $select . " FROM books";
 		         // Connect to MySQL
 		         if ( !( $database = mysqli_connect( "localhost",
-		            "root", "" ) ) )                      
+		            "root", "" ) ) )
 		            die( "Could not connect to database </body></html>" );
-		   
+
 		         // open Products database
-		         if ( !mysqli_select_db( $database, "products") )
+		         if ( !mysqli_select_db( $database, "assignment1") )
 		            die( "Could not open products database </body></html>" );
-		
+
 		         // query Products database
-		         if ( !( $result = mysqli_query( $database, $query) ) ) 
+		         if ( !( $result = mysqli_query( $database, $query) ) )
 		         {
 		            print( "Could not execute query! <br />" );
 		            die( mysqli_error() . "</body></html>" );
 		         } // end if
-		
+
 		         mysqli_close( $database );
 		      ?><!-- end PHP script -->
 		      <h3>Search Results</h3>
@@ -50,9 +50,9 @@
 		            {
 		               // build table to display results
 		               print( "<tr>" );
-		               foreach ( $row as $key => $value ) 
+		               foreach ( $row as $key => $value )
 		                  print( "<td>$value</td>" );
-		
+
 		               print( "</tr>" );
 		            } // end for
 		         ?><!-- end PHP script -->
