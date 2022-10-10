82307fe04b79351820a21c3d5fc83c4f6bf504e6
mostaleb <79292262+mostaleb@users.noreply.github.com>
2022-10-10 14:15:04 -0400

    formatted code

--- signup_server.php
+++ signup_server.php
@@ -2,62 +2,63 @@
 <html xmlns="http://www.w3.org/1999/xhtml">
 
 <head>
-    <title>Search Results</title>
-    <style type="text/css">
-    body {
-        font-family: arial, sans-serif;
-        background-color: #F0E68C
-    }
+	<title>Search Results</title>
+	<style type="text/css">
+		body {
+			font-family: arial, sans-serif;
+			background-color: #F0E68C
+		}
 
-    table {
-        background-color: #ADD8E6
-    }
+		table {
+			background-color: #ADD8E6
+		}
 
-    td {
-        padding-top: 2px;
-        padding-bottom: 2px;
-        padding-left: 4px;
-        padding-right: 4px;
-        border-width: 1px;
-        border-style: inset
-    }
-    </style>
+		td {
+			padding-top: 2px;
+			padding-bottom: 2px;
+			padding-left: 4px;
+			padding-right: 4px;
+			border-width: 1px;
+			border-style: inset
+		}
+	</style>
 </head>
 
 <body>
-    <?php
-		         extract( $_POST );
+	<?php
+	extract($_POST);
 
-		         // build SELECT query
+	// build SELECT query
 
-				 $query="INSERT INTO student (first_Name, last_Name, address, email, phone_Number, date_of_birth)
-				 VALUES ('$first_Name','$last_Name','$address','$email', '$phone_Number', '$date_of_birth')";
-
-
-		         // Connect to MySQL
-		         if ( !( $database = mysqli_connect( "localhost",
-		            "root", "" ) ) )
-		            die( "Could not connect to database </body></html>" );
-
-		         // open Products database
-		         if ( !mysqli_select_db( $database ,"assignment1" ) )
-		            die( "Could not open products database </body></html>" );
+	$query = "INSERT INTO student (first_Name, last_Name, address, email, phone_Number, date_of_birth)
+				 		 VALUES ('$first_Name','$last_Name','$address','$email', '$phone_Number', '$date_of_birth')";
 
 
+	// Connect to MySQL
+	if (!($database = mysqli_connect(
+		"localhost",
+		"root",
+		""
+	)))
+		die("Could not connect to database </body></html>");
 
-		         // query Products database
-		         if ( !( $result = mysqli_query( $database,$query) ) )
-		         {
-		            print( "Could not execute query! <br />" );
-		            // die( mysqli_error() . "</body></html>" );
-		         } // end if
-				else
-				{
-					print("$first_Name $last_Name was insterted into the Database correctly");
-				}
-		         mysqli_close( $database );
-		      ?>
-    <!-- end PHP script -->
+	// open Products database
+	if (!mysqli_select_db($database, "assignment1"))
+		die("Could not open products database </body></html>");
+
+
+
+	// query Products database
+	if (!($result = mysqli_query($database, $query))) {
+		print("Could not execute query! <br />");
+		// die( mysqli_error() . "</body></html>" );
+	} // end if
+	else {
+		print("$first_Name $last_Name was insterted into the Database correctly");
+	}
+	mysqli_close($database);
+	?>
+	<!-- end PHP script -->
 
 </body>
 
